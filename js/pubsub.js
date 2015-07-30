67dcb676984fbe79c9a9c450e56eff1f05956ed3
ShivanKaul <shivan@omsignal.com>
2015-07-29 22:58:21 -0400

    Initial commit for pubsub

---
+++ js/pubsub.js
@@ -0,0 +1,29 @@
+var gcloud = require('gcloud');
+var pubsub;
+
+// From Google Compute Engine:
+pubsub = gcloud.pubsub({
+    projectId: 'gitjob-1020',
+});
+
+// Create a new topic.
+pubsub.createTopic('projects/gitjob-1020/topics/newThread', function(err, topic) {});
+
+// Reference an existing topic.
+var topic = pubsub.topic('my-existing-topic');
+
+// Publish a message to the topic.
+topic.publish('New message!', function(err) {});
+
+// Subscribe to the topic.
+topic.subscribe('new-subscription', function(err, subscription) {
+    // Register listeners to start pulling for messages.
+    function onError(err) {}
+    function onMessage(message) {}
+    subscription.on('error', onError);
+    subscription.on('message', onMessage);
+
+    // Remove listeners to stop pulling for messages.
+    subscription.removeListener('message', onMessage);
+    subscription.removeListener('error', onError);
+});
