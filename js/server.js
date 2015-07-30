a44e14a6bcbe7318f19c6c4d175bad1841d4fda4
ShivanKaul <shivan@omsignal.com>
2015-07-30 18:47:13 -0400

    Heroku config vars

--- js/server.js
+++ js/server.js
@@ -1,16 +1,20 @@
 var gcloud = require('gcloud');
 var pubsub;
 
-// From Google Compute Engine:
 pubsub = gcloud.pubsub({
     projectId: 'gitjob-1020',
+    credentials: {
+        private_key: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL
+    }
 });
+
 
 // Create a new topic.
 pubsub.createTopic('projects/gitjob-1020/topics/newThread', function(err, topic) {});
 
 // Reference an existing topic.
-var topic = pubsub.topic('my-existing-topic');
+var topic = pubsub.topic('newThread');
 
 // Publish a message to the topic.
 topic.publish('New message!', function(err) {});
