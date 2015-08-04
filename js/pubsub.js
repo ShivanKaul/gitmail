8d454d4016abcc7c2a5f96c2a010c2ef3e7f5c72
ShivanKaul <shivan@omsignal.com>
2015-08-03 23:27:36 -0400

    Try to receive message on Heroku URL

--- js/pubsub.js
+++ js/pubsub.js
@@ -1,44 +1,110 @@
 var gcloud = require('gcloud');
-var pubsub;
+var request = require('request');
 
-pubsub = gcloud.pubsub({
+var pubsub = gcloud.pubsub({
     projectId: 'gitjob-1020',
     credentials: {
         private_key: process.env.PRIVATE_KEY,
+        //private_key_id: process.env.PRIVATE_KEY_ID,
+        //client_id: process.env.CLIENT_ID,
+        //type: process.env.TYPE,
         client_email: process.env.CLIENT_EMAIL
     }
+    //keyFilename: __dirname + '/../gitjob-7997d2f90025.json'
 });
 
+exports.start = function () {
 
 // Create a new topic.
-pubsub.createTopic('projects/gitjob-1020/topics/newThread', function(err, topic, apiResponse) {
-    console.log("api response on creating topic is " + apiResponse)
-    console.log("topic on creating topic is " + topic)
-});
+    pubsub.createTopic('newThread7', function (err, topic, apiResponse) {
+        console.log("trying to create topic")
+        if (err) {
+            console.log("Couldn't create a new topic because " + err)
+        }
+        else {
+            console.log("no error while creating topic!")
+            console.log("api response on creating topic is ")
+            console.log(apiResponse)
+            console.log("topic on creating topic is ")
+            console.log(topic)
+            // Subscribe to the topic.
+            topic.subscribe('new-subscription7', function (err, subscription, apiResponse) {
+                // Register listeners to start pulling for messages.
+                function onMessage(message) {
+                    console.log("Message is ");
+                    console.log(message);
+                }
 
+                function onError(err) {
+                    console.log("Got an error :( " + err);
+                }
+                if (err) {
+                    console.log("Received error while subscribing to subscription " + err)
+                }
+                else {
+                    console.log("No error while creating new subscription!")
+                    subscription.on('error', onError);
+                    subscription.on('message', onMessage);
+
+                    // Remove listeners to stop pulling for messages.
+                    //subscription.removeListener('message', onMessage);
+                    //subscription.removeListener('error', onError);
+                }
+            });
+        }
+    });
+
+    // Watch request
+    //request.post({
+    //
+    //    url: 'https://www.googleapis.com/gmail/v1/users/me/watch',
+    //    headers: {
+    //        'content-type': 'application/json',
+    //        'Authorization': 'Bearer '+ " "
+    //    },
+    //    scope: [
+    //        'https://mail.google.com/'
+    //    ],
+    //
+    //    'body': JSON.stringify({
+    //        'topicName': "/projects/gitjob-1020/topics/newThread7",
+    //        'labelIds': ["INBOX"]
+    //    })
+    //}, function(error, resp, body){
+    //    if (error) {
+    //        console.log("Got error while sending watch request request");
+    //        console.log(error)
+    //    }
+    //    else {
+    //        console.log("Got response!");
+    //        console.log(resp)
+    //    }
+    //});
 // Reference an existing topic.
-var topic = pubsub.topic('projects/gitjob-1020/topics/newThread');
+//    var topic = pubsub.topic('newThread6');
+
+//topic.('pubsub_policy.json');
+
+
+};
+
+exports.newThread = function () {
+    var topic = pubsub.topic('newThread');
 
 // Publish a message to the topic.
-topic.publish('New message!', function(err) {
+    topic.publish({
+        data: 'New message!'
+    }, function (err) {
+        if (err) {
+            console.log("Got error in publishing message to topic" + err)
+        }
+        else {
+            console.log("Published message successfully!")
+        }
+    });
+};
 
-});
+exports.receivedMessage = function (req) {
+    console.log("message!")
 
-topic.('pubsub_policy.json');
-
-// Subscribe to the topic.
-topic.subscribe('new-subscription', function(err, subscription, apiResponse) {
-    // Register listeners to start pulling for messages.
-    function onError(err) {
-        console.log("Got an error :( " + err);
-    }
-    function onMessage(message) {
-        console.log("Message is " + message);
-    }
-    subscription.on('error', onError);
-    subscription.on('message', onMessage);
-
-    // Remove listeners to stop pulling for messages.
-    subscription.removeListener('message', onMessage);
-    subscription.removeListener('error', onError);
-});
+};
--- js/server.js
+++ js/server.js
@@ -1,32 +1,80 @@
 // Load the http module to create an http server.
 var http = require('http');
 var fs = require('fs');
+var request = require('request');
+var pubsub = require('./pubsub.js');
+
+// Watch command
+
+console.log("Sending watch command...");
+
+//request.post({
+//
+//    url: 'https://www.googleapis.com/gmail/v1/users/me/watch',
+//    headers: {
+//        'content-type': 'application/json',
+//        'Authorization': 'Bearer '+ " "
+//    },
+//    scope: [
+//        'https://mail.google.com/'
+//    ],
+//
+//    'body': JSON.stringify({
+//        'topicName': "/projects/gitjob-1020/topics/newThread",
+//        'labelIds': ["INBOX"]
+//    })
+//}, function(error, resp, body){
+//    if (error) {
+//        console.log("Got error while sending watch request request");
+//        console.log(error)
+//    }
+//    else {
+//        console.log("Got response!");
+//        console.log(resp)
+//    }
+//});
 
 var server = http.createServer(function (req, responseToSend) {
 
+    //pubsub.run();
+
+
     var file = req.url;
-
-    var filePrefix = '/../htdocs';
-
+    //
+    //var filePrefix = '/../htdocs';
+    //
     if (file == '/') {
-        file = '/index.html';
+        pubsub.start();
+        //console.log("HIT END POINT, REQ IS");
+        //console.log(req);
+        //pubsub.receivedMessage(req);
     }
+    //else if (file == '/newThread') {
 
-    if (file == '/googlec2f8f617ad7e80e0.html') {
-        file = '/googlec2f8f617ad7e80e0.html';
-    }
+        //console.log("HIT END POINT, REQ IS");
+        //console.log(req);
+        ////pubsub.newThread();
+        //pubsub.receivedMessage(req);
+    //}
 
-    fs.readFile(__dirname + filePrefix + file,
-        function (err, data) {
-            if (err) {
-                console.log(err);
-                responseToSend.writeHead(500);
-                return responseToSend.end('Error loading index.html');
-            }
+    responseToSend.end();
 
-            responseToSend.writeHead(200);
-            responseToSend.end(data);
-        });
+    //
+    //if (file == '/googlec2f8f617ad7e80e0.html') {
+    //    file = '/googlec2f8f617ad7e80e0.html';
+    //}
+    //
+    //fs.readFile(__dirname + filePrefix + file,
+    //    function (err, data) {
+    //        if (err) {
+    //            console.log(err);
+    //            responseToSend.writeHead(500);
+    //            return responseToSend.end('Error loading index.html');
+    //        }
+    //
+    //        responseToSend.writeHead(200);
+    //        responseToSend.end(data);
+    //    });
 
 });
 
